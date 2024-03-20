8824b15d56aef6230ecbb62ba3c742e95713dc21
swiftleah <leahrosehenry@gmail.com>
2024-03-20 12:00:44 +0000

    Task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- lists all bands with Glam rock style ranked by longevity
+-- from metal_band.sql
+SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
